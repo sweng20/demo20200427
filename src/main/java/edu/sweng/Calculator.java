d2da2d7e441c2d3221d069d438616e7a63407983
Philipp Toggweiler <philipp.toggweiler@hslu.ch>
2020-04-27 16:32:14 +0200

    add new class Calculator containing an add() method

---
+++ src/main/java/edu/sweng/Calculator.java
@@ -0,0 +1,18 @@
+package edu.sweng;
+
+public class Calculator {
+
+
+    /**
+     * Calculate the sum of two terms
+     * @param a term1
+     * @param b term2
+     * @return sum a + b
+     */
+    public double add(double a, double b) {
+
+        return a + b;
+
+    }
+
+}
